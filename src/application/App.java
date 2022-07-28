5d83785ef57255d853f9925d93c0f60dd97ed6ce
earl-stephens <stephensearl@hotmail.com>
2022-07-27 20:09:17 -0600

    List the four types of numeric types in Java

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,25 @@ package application;
 
 public class App {
 	public static void main(String[] args) {
+		boolean bValue = true;
+		char c = '4';
+		
+		//integer numeric types
+		/* byte, short, integer, long - each one sets aside a certain
+		 * number of bytes in memory
+		 * byte = 8 bits
+		 * -128 to 127
+		 * short = 62k approx
+		 * integer = normal numbers
+		 * long = for very long values; have to append an 'L'
+		 * to the end, cuz Java initially thinks it's an int
+		 * 
+		 */
+		
+		byte byteValue = 123;
+		short shortValue = 30000;
+		int intValue = 423434;
+		long longValue = 34235436784848L;
 		
 	}
 }
